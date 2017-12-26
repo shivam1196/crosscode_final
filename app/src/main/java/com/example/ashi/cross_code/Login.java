2d88f3a1bc4fd36a54784b8fe4f8fce8f8ca96bc
Ashi <500052000@stu.upes.ac.in>
2017-12-26 21:50:11 +0530

    last changes

--- app/src/main/java/com/example/ashi/cross_code/Login.java
+++ app/src/main/java/com/example/ashi/cross_code/Login.java
@@ -10,8 +10,8 @@ import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     Button login;
-    EditText name,sapid;
-    String e,p;
+    EditText name,sapid,otp;
+    String e,p,o;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,22 +19,30 @@ public class Login extends AppCompatActivity {
         login=findViewById(R.id.login);
         name=findViewById(R.id.email);
         sapid=findViewById(R.id.password);
+        otp=findViewById(R.id.otp);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 e = name.getText().toString();
                 p = sapid.getText().toString();
+                o = otp.getText().toString();
                 if (e.equals("") || p.equals("")) {
                     Toast.makeText(Login.this, "Please enter Name and Sapid", Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
-                    Intent i = new Intent(Login.this, Rules.class);
-                    Bundle b=new Bundle();
-                    b.putString("name",e);
-                    b.putString("sapid",p);
-                    i.putExtras(b);
-                    startActivity(i);
+                    if(o.equals("88552277"))
+                    {
+                        Intent i = new Intent(Login.this, Rules.class);
+                        Bundle b = new Bundle();
+                        b.putString("name", e);
+                        b.putString("sapid", p);
+                        i.putExtras(b);
+                        startActivity(i);
+                    }
+                    else{
+                        Toast.makeText(Login.this, "Please enter Correct otp", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
